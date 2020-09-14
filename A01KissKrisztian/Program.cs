e5ac4e9473a8b2963371351bf7648deae3bbde99
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-09-14 10:58:49 +0200

    Beolvasás metódus létrehozása

--- A01KissKrisztian/Program.cs
+++ A01KissKrisztian/Program.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace A01KissKrisztian
 {
     class Program
     {
+        static List<int> Lista = new List<int>();
+        static void Beolvasas()
+        {
+            int seged;
+            StreamReader olvas = new StreamReader("Adatok.dat");
+            while (!olvas.EndOfStream)
+            {
+                seged = int.Parse(olvas.ReadLine()) * 2;
+                Lista.Add(seged);
+            }
+            olvas.Close();
+        }
         static void Main(string[] args)
         {
-
+            Beolvasas();
+            Console.ReadLine();
         }
     }
 }
